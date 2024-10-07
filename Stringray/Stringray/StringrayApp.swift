5b28ec87628f5623b48c0147be67d677419548a5
stixlee <michael.lee.1983@iclllud.com>
2024-10-07 14:51:30 -0600

    Initial Commit

---
+++ Stringray/Stringray/StringrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  StringrayApp.swift
+//  Stringray
+//
+//  Created by Michael Lee on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct StringrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Stringray/StringrayTests/StringrayTests.swift
@@ -0,0 +1,17 @@
+//
+//  StringrayTests.swift
+//  StringrayTests
+//
+//  Created by Michael Lee on 10/7/24.
+//
+
+import Testing
+@testable import Stringray
+
+struct StringrayTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
